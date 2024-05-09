b68741416012dce94430577611290092493180fb
mwwl <moniquewee26@gmail.com>
2024-05-09 18:05:38 +1000

    Added scheduled method to check for non-responsiveness of services

--- client-new/src/main/java/com/distalgo/client/service/ClientConsumerService.java
+++ client-new/src/main/java/com/distalgo/client/service/ClientConsumerService.java
@@ -1,17 +1,21 @@
 package com.distalgo.client.service;
 
 import com.distalgo.saga.events.CallbackEvent;
-import com.distalgo.saga.events.InventoryEvent;
 import jakarta.annotation.PostConstruct;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @Service
+@EnableScheduling
 public class ClientConsumerService {
-    // note: consuming callback event
     private final ReactiveKafkaConsumerTemplate<String, CallbackEvent> callbackEventConsumerKafkaTemplate;
 
     @Autowired
@@ -25,21 +29,46 @@ public class ClientConsumerService {
      * Runs automatically after application context has been initialized
      */
     @PostConstruct
-    public void startConsumingOrderEvents() {
+    public void startConsumingCallbackEvents() {
         consumeCallbackEvent().subscribe();
     }
 
     /**
      * Consumed callback event, to update the GUI accordingly
-     * @return
+     *
+     * Have to filter out the messages that were meant for this person -- using sessionID (have to think how to get the sessionID though...)
      */
     private Flux<CallbackEvent> consumeCallbackEvent() {
         return callbackEventConsumerKafkaTemplate
                 .receiveAutoAck()
                 .map(ConsumerRecord::value)
+                .filter(callbackEvent -> callbackEvent.getSessionID().equals(clientService.getSessionID()))
                 .doOnNext(callbackEvent -> {
                     System.out.println("within ClientConsumerService, received: " + callbackEvent);
                     clientService.updateClient(callbackEvent);
                 });
+    }
+
+    /**
+     * Checks every 30s if a set time has passed since the last published message, of the client, and
+     *
+     * Testing every 10 seconds for now
+     */
+    @Scheduled(fixedDelay = 10000)
+    private void checkUnconsumed() {
+        System.out.println("checking unconsumed");
+        Instant lastTimestamp = clientService.getLastPublishedTime();
+        Instant currentTimeStamp = Instant.now();
+        String clientUpdate = "One of the services is down, your tickets are tentatively booked, " +
+                "pending seat availability and/or payment.\n";
+
+        if (lastTimestamp != null) {
+            Duration timePassed = Duration.between(lastTimestamp, currentTimeStamp);
+            if (timePassed.toMillis() > 10000) {
+                System.out.println("current time stamp: " + currentTimeStamp);
+                clientService.updateClient(clientUpdate);
+                clientService.saveCurrentTime(null);
+            }
+        }
     }
 }
