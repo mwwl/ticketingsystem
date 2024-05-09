c7486bad696e79aff459394f90e7a0afc1a05a0e
mwwl <moniquewee26@gmail.com>
2024-05-09 18:04:00 +1000

    Changed group id to be entered dynamically

--- client-new/src/main/java/com/distalgo/client/ClientApplication.java
+++ client-new/src/main/java/com/distalgo/client/ClientApplication.java
@@ -1,13 +1,11 @@
 package com.distalgo.client;
 
-import com.distalgo.client.gui.ClientGUI;
-//import com.distalgo.client.service.ClientService;
 import com.distalgo.client.service.ClientPublisherService;
 import com.distalgo.client.service.ClientService;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,9 +13,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.io.File;
-import java.util.Map;
+import org.springframework.core.env.Environment;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -26,45 +22,58 @@ import java.util.UUID;
 public class ClientApplication implements ApplicationRunner {
     private final static int IP_IDX = 0;
     private final static int PORT_IDX = 1;
+    private static String sessionID = null;
+//    private static String clientPort = null;
 
     @Autowired
-    ClientPublisherService publisherService;
+    private Environment environment;
 
+    @Autowired
+    private ClientPublisherService publisherService;
+
+    @Autowired
+    private ClientService client;
+
+
+    /**
+     * sets the clientPort and groupID of the client before running the application
+     *
+     * (each client has a different group ID to ensure each client listens to all messages)
+     * https://www.infoworld.com/article/3694893/optimize-apache-kafka-by-understanding-consumer-groups.html#:~:text=Kafka%20consumers%20are%20generally%20configured,depends%20on%20your%20use%20case
+     *
+     *
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+        ClientApplication clientApplication = new ClientApplication();
         System.setProperty("java.awt.headless", "false"); // to enable to show the GUI
+
+        clientApplication.checkNumArguments(args);
+        String clientPort = args[PORT_IDX];
+        clientApplication.checkPortValidity(clientPort);
+
+        sessionID = UUID.randomUUID().toString();
+        System.setProperty("server.port", clientPort);
+
+        String groupID = String.format("client-%s", sessionID);
+        System.out.println("group id: " + groupID);
+        System.setProperty("spring.kafka.consumer.group-id", groupID);
+
         SpringApplication.run(ClientApplication.class, args);
     }
 
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        ClientService client = new ClientService();
-        String[] arguments = args.getSourceArgs();
-
-        System.out.println("arguments provided: ");
-        for (String s: arguments) {
-            System.out.println(s);
-        }
-
-        ObjectMapper objectMapper = new YAMLMapper();
-        checkNumArguments(arguments);
-        checkPortValidity(arguments[PORT_IDX]);
-
-        String sessionID = UUID.randomUUID().toString();
-        String clientIP = arguments[IP_IDX];
-        String clientPort = arguments[PORT_IDX];
-
-        Map<String, Object> user = objectMapper.readValue(new File("/Users/moniquew/Downloads/ticketingsystem/client-new/src/main/resources/application.yml"),
-                new TypeReference<Map<String, Object>>() { });
-
-        Map<String, Object> server = (Map<String, Object>) user.get("server");
-
-        System.out.println("old port:" + server.get("port"));
-        server.put("port", clientPort);
         System.out.println("publisher:" + publisherService);
+        System.out.println("sessionID for this client: " + sessionID);
+        client.setSessionID(sessionID);
         client.launchGUI(sessionID, publisherService);
-//        ClientGUI clientGUI = new ClientGUI(sessionID, clientIP, clientPort);
     }
 
+    // error handling if number of arguments input is wrong, and if the port is not valid
+    // but have to handle the case where the same input is given though
     private void checkNumArguments(String[] args) {
         if (args.length != 2) {
             System.err.println("Please enter the correct number of arguments.");
